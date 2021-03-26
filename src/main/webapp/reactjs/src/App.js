63bda3f9a0ac8d366b008b25f4f50dbd7efd0f29
Jelena Mihajlovic <starglitz47@gmail.com>
2021-03-26 23:35:45 +0100

    added a sample page redirection using react route

--- src/main/webapp/reactjs/src/App.js
+++ src/main/webapp/reactjs/src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
-import Navbar from './components/navbar.js';
+import Navbar from './components/navbar';
+import SampleLayout from './layouts/SampleLayout';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+        <Router>
+            <Switch>
+                <Route path="/samplePath" exact component={SampleLayout}/>
+            </Switch>
+        </Router>
     </div>
   );
 }
---
+++ src/main/webapp/reactjs/src/layouts/SampleLayout.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SampleLayout = () => {
+    return (
+    <>
+    <p>This is sample page redirection </p>
+    </>
+   );
+};
+
+export default SampleLayout;
