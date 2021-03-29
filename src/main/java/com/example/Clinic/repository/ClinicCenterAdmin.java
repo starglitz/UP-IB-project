62ec02ad193cd29738f2656a673af073d5d3d50f
Jelena Mihajlovic <starglitz47@gmail.com>
2021-03-29 10:39:02 +0200

    small fix in clinic center admin repository

--- src/main/java/com/example/Clinic/repository/ClinicCenterAdmin.java
+++
@@ -1,6 +0,0 @@
-package com.example.Clinic.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClinicCenterAdmin extends JpaRepository<ClinicCenterAdmin, Long> {
-}
