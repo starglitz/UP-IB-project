94ec5d7a473bd5b0e9869b1872ff90c47bf768b2
Jelena Mihajlovic <starglitz47@gmail.com>
2021-04-03 14:08:38 +0200

    commit

--- src/main/webapp/reactjs/src/layouts/RegisterRequests.js
+++ src/main/webapp/reactjs/src/layouts/RegisterRequests.js
@@ -1,5 +1,5 @@
 import React, {Component, useEffect, useState} from 'react';
-
+import { DataGrid } from '@material-ui/data-grid';
 const RegisterRequests = () => {
 
     const [requests, setRequests] = useState({});
@@ -19,8 +19,39 @@ const RegisterRequests = () => {
 
 
 
+    const columns = [
+        { field: 'register_request_id', headerName: 'Request ID', width: 130 },
+        { field: 'patient', headerName: 'Patient', width: 130 },
+        { field: 'status', headerName: 'Status', width: 130 }
+    ];
+
+console.log(requests);
+    const rows = [requests];
+
+console.log(rows);
     return (
         <>
+           <table>
+               <thead>
+               <tr>
+                   <td>ID</td>
+                   <td>Name & Surname</td>
+                   <td>Status</td>
+               </tr>
+               </thead>
+                <tbody>
+                   {[requests].map((req) => (
+                       // <tr key={req.id}>
+                       // <td key={req.id}>{req.id}</td>
+                       // <td key={req.id}>{req.email}</td>
+                       // <td key={req.id}>{req.name}</td>
+                       // </tr>
+                        <p key={req.id}>{req.name}</p>
+                   ))}
+                </tbody>
+
+           </table>
+            {/*<DataGrid rows={rows} columns={columns} pageSize={5} checkboxSelection />*/}
             <span>{JSON.stringify(requests)}</span>
         </>
     );
