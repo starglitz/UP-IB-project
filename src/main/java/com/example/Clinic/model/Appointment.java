a3608294a17b910adc1e24223dc31679df69b3b3
Jelena Mihajlovic <starglitz47@gmail.com>
2021-04-12 12:05:06 +0200

    modified backend for appointment to work properly

--- src/main/java/com/example/Clinic/model/Appointment.java
+++ src/main/java/com/example/Clinic/model/Appointment.java
@@ -4,7 +4,9 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 @Data
 @NoArgsConstructor
@@ -13,13 +15,15 @@ public class Appointment {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long appointment_id;
-    private AppointmentStatus status;
-    @Column(name = "local_date_time", columnDefinition = "TIMESTAMP")
-    private LocalDateTime dateAndTime;
+    private AppointmentStatus status = AppointmentStatus.FREE;
+    private LocalDate date;
+    private LocalTime time;
     private String duration;
 
     @ManyToOne
     private Doctor doctor;
+
+    //(cascade = CascadeType.PERSIST)
     @ManyToOne
     private Nurse nurse;
     private int price;
--- src/main/java/com/example/Clinic/rest/impl/AppointmentApiImpl.java
+++ src/main/java/com/example/Clinic/rest/impl/AppointmentApiImpl.java
@@ -1,20 +1,31 @@
 package com.example.Clinic.rest.impl;
 
 import com.example.Clinic.model.Appointment;
+import com.example.Clinic.model.Doctor;
+import com.example.Clinic.model.Nurse;
 import com.example.Clinic.rest.AppointmentApi;
 import com.example.Clinic.service.AppointmentService;
+import com.example.Clinic.service.DoctorService;
+import com.example.Clinic.service.NurseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
 import javax.validation.Valid;
+import java.util.Optional;
 
 @Component
 public class AppointmentApiImpl implements AppointmentApi {
 
     @Autowired
     private AppointmentService appointmentService;
+
+    @Autowired
+    private DoctorService doctorService;
+
+    @Autowired
+    private NurseService nurseService;
 
     @Override
     public ResponseEntity getAllAppointments() {
@@ -31,6 +42,12 @@ public class AppointmentApiImpl implements AppointmentApi {
 
     @Override
     public ResponseEntity<Appointment> addAppointment(@Valid Appointment appointment) {
+        Doctor doctor = doctorService.findById(appointment.getDoctor().getId()).get();
+        appointment.setDoctor(doctor);
+        Nurse nurse = nurseService.findById(appointment.getNurse().getId()).get();
+        appointment.setNurse(nurse);
+
+        System.out.println("date and time!!!!!!!!!!!!!!!!!!!!: "+ appointment.getDateAndTime());
         return new ResponseEntity(appointmentService.add(appointment), HttpStatus.OK);
     }
 
