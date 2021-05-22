769291ad42f190928340e3c314b11a880d659c37
Boris Jancic <jancicboris56@gmail.com>
2021-05-22 11:44:35 +0200

    Added missing annotation

--- src/main/java/com/example/Clinic/service/impl/AddressServiceImpl.java
+++ src/main/java/com/example/Clinic/service/impl/AddressServiceImpl.java
@@ -4,9 +4,11 @@ import com.example.Clinic.model.Address;
 import com.example.Clinic.repository.AddressRepository;
 import com.example.Clinic.service.AddressService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+@Service
 public class AddressServiceImpl implements AddressService {
 
     @Autowired
