5b61a0cb71ebc105492217bfc4fce6b354570067
Boris Jancic <jancicboris56@gmail.com>
2021-06-07 12:29:43 +0200

    Defined certificate properties

--- src/main/java/com/example/Clinic/security/config/SecurityConfiguration.java
+++ src/main/java/com/example/Clinic/security/config/SecurityConfiguration.java
@@ -76,6 +76,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.requiresChannel().anyRequest().requiresSecure();
         http.cors().configurationSource(corsConfigurationSource());
         http.cors().and().csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
