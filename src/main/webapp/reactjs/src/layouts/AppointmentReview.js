d7952a296bea82fcf2109be6638f9ff260779a14
Boris Jancic <jancicboris56@gmail.com>
2021-07-04 20:27:26 +0200

    Added patients medical history view during the appointment

--- src/main/webapp/reactjs/src/layouts/AppointmentReview.js
+++ src/main/webapp/reactjs/src/layouts/AppointmentReview.js
@@ -2,11 +2,13 @@ import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'react-calendar/dist/Calendar.css';
 import AppointmentReviewBox from "../components/appointments/AppointmentReviewBox";
+import {PatientIllnessTable} from "../components/patients/PatientIllnessTable";
 
 const AppointmentReview = () => {
     return (
             <>
-                <AppointmentReviewBox id="9"/>
+                <AppointmentReviewBox />
+                <PatientIllnessTable />
             </>
     );
 };
