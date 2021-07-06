5f31600dd661c2bf62c1d2c101109af96339a400
Boris Jancic <jancicboris56@gmail.com>
2021-07-06 22:22:54 +0200

    Implemented geting recipes for specific nurses

--- src/main/java/com/example/Clinic/rest/impl/RecipeApiImpl.java
+++ src/main/java/com/example/Clinic/rest/impl/RecipeApiImpl.java
@@ -5,22 +5,21 @@ import com.example.Clinic.model.PatientBook;
 import com.example.Clinic.model.Recipe;
 import com.example.Clinic.rest.RecipeApi;
 import com.example.Clinic.rest.support.converter.DtoToRecipe;
+import com.example.Clinic.rest.support.converter.RecipeToDto;
 import com.example.Clinic.rest.support.dto.RecipeDto;
+import com.example.Clinic.service.NurseService;
 import com.example.Clinic.service.PatientBookService;
 import com.example.Clinic.service.RecipeService;
-import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.xml.sax.SAXException;
 
 import javax.validation.Valid;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,10 +27,16 @@ import java.util.List;
 public class RecipeApiImpl implements RecipeApi {
 
     @Autowired
+    private NurseService nurseService;
+
+    @Autowired
     private RecipeService recipeService;
 
     @Autowired
     private DtoToRecipe dtoToRecipe;
+
+    @Autowired
+    private RecipeToDto recipeToDto;
 
     @Autowired
     private PatientBookService patientBookService;
@@ -74,14 +79,20 @@ public class RecipeApiImpl implements RecipeApi {
     @Override
     public ResponseEntity getAllRecipes() {
         System.out.println("recepti");
-        List<Recipe> recipes = recipeService.getAllRecipes();
-        return new ResponseEntity(recipes, HttpStatus.OK);
+        return new ResponseEntity(recipeToDto.convert(recipeService.getAllRecipes()), HttpStatus.OK);
+    }
+
+    @Override
+    public ResponseEntity getNurseRecipes(Long id) {
+        Nurse nurse = nurseService.findById(id);
+        if (nurse != null)
+            return new ResponseEntity<>(recipeToDto.convert(recipeService.getNurseRecipes(nurse)), HttpStatus.OK);
+        return new ResponseEntity<>(null, HttpStatus.OK);
     }
 
     @Override
     public ResponseEntity getNotApprovedRecipes() {
         System.out.println("ne odobreni recepti");
-        List<Recipe> recipes = recipeService.getNotApprovedRecipes();
-        return new ResponseEntity(recipes, HttpStatus.OK);
+        return new ResponseEntity<>(recipeToDto.convert(recipeService.getNotApprovedRecipes()), HttpStatus.OK);
     }
 }
