a58851ff847339bc5257cf7fc0859129e2c39c9f
Jelena Mihajlovic <starglitz47@gmail.com>
2021-07-06 22:28:20 +0200

    fixed maps to work better

---
+++ src/main/webapp/reactjs/src/layouts/my_great_place.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import shouldPureComponentUpdate from 'react-pure-render/function';
+import PropTypes from 'prop-types';
+import {greatPlaceStyle} from './my_great_place_styles.js';
+
+export default class MyGreatPlace extends Component {
+    static propTypes = {
+        text: PropTypes.string
+    };
+
+    static defaultProps = {};
+
+    shouldComponentUpdate = shouldPureComponentUpdate;
+
+    render() {
+        return (
+            <div style={greatPlaceStyle}>
+                {this.props.text}
+            </div>
+        );
+    }
+}
